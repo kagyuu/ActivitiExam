b7e051f482da3f47b34e8e1edb67a894b58f2b92
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2015-12-02 01:00:38 +0900

    I was succeeded to deploy job.

---
+++ src/main/java/com/snail/exam/MyProcess.java
@@ -0,0 +1,24 @@
+package com.snail.exam;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyProcess {
+
+	private static Logger log = LoggerFactory.getLogger(MyProcess.class);
+	public static void main(String[] args) {
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		repositoryService.createDeployment()
+		  .addClasspathResource("org/activiti/test/VacationRequest.bpmn20.xml")
+		  .deploy();
+
+		log.info("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());
+
+
+	}
+
+}
