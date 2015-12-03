eaa5ed3dad72ef34413bc6acf1329023f8d193f6
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2015-12-04 01:59:10 +0900

    Append logback.xml

--- src/main/java/com/snail/exam/MyProcess.java
+++ src/main/java/com/snail/exam/MyProcess.java
@@ -11,6 +11,8 @@ public class MyProcess {
 	private static Logger log = LoggerFactory.getLogger(MyProcess.class);
 	public static void main(String[] args) {
 		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		
+		// 4.3.1. Deploying the process
 		RepositoryService repositoryService = processEngine.getRepositoryService();
 		repositoryService.createDeployment()
 		  .addClasspathResource("org/activiti/test/VacationRequest.bpmn20.xml")
