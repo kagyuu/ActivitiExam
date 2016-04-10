5de6e321fa0aac8650251fcff770fe3e6ccac941
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2016-04-11 00:10:50 +0900

    Add mail task example

---
+++ src/main/java/com/snail/exam/MyProcess3.java
@@ -0,0 +1,31 @@
+package com.snail.exam;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyProcess3 {
+
+    private static final Logger log = LoggerFactory.getLogger(MyProcess.class);
+
+    public static void main(String[] args) {
+        try {
+            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+            RepositoryService repositoryService = processEngine.getRepositoryService();
+            repositoryService.createDeployment()
+                    .addClasspathResource("org/activiti/test/EmailProcess.bpmn")
+                    .deploy();
+            
+            RuntimeService runtimeService = processEngine.getRuntimeService();
+            ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("sendEmail");            
+
+        } catch (Throwable th) {
+            log.error("ERROR", th);
+        }
+    }
+}
